95e088b0db890877f689491a924645107a6a67bf
gotanel1 <gotscot502@gmail.com>
2017-03-09 18:46:35 +0700

    Update Shiftbit.cpp

--- codes/Shiftbit.cpp
+++ codes/Shiftbit.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-main(void){
+void testBit(int d){
+	int x = (1<<3);
+	int a = (d & 1);
+	cout << "Test Shift " << d <<endl;
+	cout << "Test bit 3 = 1 is " << ((d & x)==x)<<endl;
+	/*if(a==1){
+		cout << "\nOdd Number";
+	}else{
+		cout << "\nEven Number";
+	}*/
+	(a==1)?(cout<<"Odd"):(cout<<"Even");
+}
 
+main(void){
+testBit(10);
 return 0;
 }
